ceaadede180781d96b6cebf1f4132a18a6dacad4
Lucas Lima <lucasbezerra14.ll@gmail.com>
2025-03-10 14:16:06 -0300

    build: estrutura inicial dos arquivos

---
+++ functions.h
@@ -0,0 +1,19 @@
+#indef __FUNCTIONS_H__
+#define __FUNCTIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct No {
+  char *name;
+  int matricula;
+  struct No *prev;
+  struct No *next;
+} No;
+
+No* makeList();
+No* insert(No *lista, int mat, char *nome); 
+No* remove(No *lista, int mat);
+No* findMat(No *lista, int mat);
+No* findName(No *lista, char *nome); 
+int showSize(No *lista);
